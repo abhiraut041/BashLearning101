81ac98161d06121892b12ec72365653a0ff9c103
Abhijeet Raut <38391991+abhiraut041@users.noreply.github.com>
2025-11-12 22:43:15 +0530

    Update check_weather.sh
    Added initial api calls for weather check api

--- check_weather.sh
+++ check_weather.sh
@@ -8,8 +8,9 @@ declare -r text_red="$(tput setaf 1)$(tput bold)"
 declare -r text_reset="$(tput sgr0)"
 declare -r line="${text_blue}############################################${text_reset}"
 declare -r SCRIPT_DIR="$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" && pwd )"
-### temp file to store/process results of geocode api for latitude/longitude
+### temp file to store/process results api calls
 declare -r GEOCODE_JSON_FILE="${SCRIPT_DIR}/.geocode_temp.json"
+declare -r WEATHER_JSON_FILE="${SCRIPT_DIR}/.weather_temp.json"
 
 
 ### URLs for API calls
@@ -39,8 +40,9 @@ if [[ "${result_count}" == 0 ]] then
 fi
 
 ### proceed with execution if city found
-echo -e "City Located...\n${line}"
+echo -e "\t\tCity Details\n${line}"
 ### get the data in variables
+cname="$( cat "${GEOCODE_JSON_FILE}" | jq '.results[]' | jq '.name' )"
 latitude="$( cat "${GEOCODE_JSON_FILE}" | jq '.results[]' | jq '.latitude' )"
 longitude="$( cat "${GEOCODE_JSON_FILE}" | jq '.results[]' | jq '.longitude' )"
 elevation="$( cat "${GEOCODE_JSON_FILE}" | jq '.results[]' | jq '.elevation' )"
@@ -49,26 +51,21 @@ country="$( cat "${GEOCODE_JSON_FILE}" | jq '.results[]' | jq '.country' )"
 population="$( cat "${GEOCODE_JSON_FILE}" | jq '.results[]' | jq '.population' )"
 
 ### display the output
-echo -e "\tCity \t\t: ${text_cyan}${city_name}${text_reset}"
+echo -e "\tCity \t\t: ${text_cyan}${cname}${text_reset}"
 echo -e "\tState \t\t: ${text_cyan}${city_state}${text_reset}"
 echo -e "\tCountry \t: ${text_cyan}${country}${text_reset}"
 echo -e "\tLongitude \t: ${text_cyan}${longitude}${text_reset}"
 echo -e "\tLatitude \t: ${text_cyan}${latitude}${text_reset}"
-echo -e "\tElevation \t: ${text_cyan}${elevation}${text_reset}"
+echo -e "\tElevation \t: ${text_cyan}${elevation} m.${text_reset}"
 echo -e "\tPopulation \t: ${text_cyan}${population}${text_reset}"
+echo "${line}"
 
+### Now use the latitude and longitude to fetch weather
+curl "${weatherapi_url}" -G --data-urlencode "latitude=${latitude}" --data-urlencode "longitude=${longitude}" --data-urlencode 'current=temperature_2m,rain,snowfall,wind_speed_10m,relative_humidity_2m' -o "${WEATHER_JSON_FILE}" -s
 
-
+cat "${WEATHER_JSON_FILE}" | jq
 
 ### Cleanup Section
 ### remove the temp file created during execution
 # rm "${SCRIPT_DIR}/.geocode_temp.json"
 
-
-
-#curl 'https://geocoding-api.open-meteo.com/v1/search?name=Mumbai&count=1&language=en&format=json' | jq
-
-#cat sample_geocode.json | jq '.results[]' | jq '.name, .admin1, .country, .latitude, .longitude, .population, .elevation ' 
-
-
-#https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature
