317135942f0e3bd1495b5783ca514313339df542
Abhijeet Raut <38391991+abhiraut041@users.noreply.github.com>
2025-11-13 20:10:09 +0530

    Update check_weather.sh
    Added functionality to display weather and city details.

--- check_weather.sh
+++ check_weather.sh
@@ -8,6 +8,7 @@ declare -r text_red="$(tput setaf 1)$(tput bold)"
 declare -r text_reset="$(tput sgr0)"
 declare -r line="${text_blue}############################################${text_reset}"
 declare -r SCRIPT_DIR="$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" && pwd )"
+declare -r tm=0.5
 ### temp file to store/process results api calls
 declare -r GEOCODE_JSON_FILE="${SCRIPT_DIR}/.geocode_temp.json"
 declare -r WEATHER_JSON_FILE="${SCRIPT_DIR}/.weather_temp.json"
@@ -40,14 +41,14 @@ if [[ "${result_count}" == 0 ]] then
 fi
 
 ### proceed with execution if city found
-echo -e "\t\tCity Details\n${line}"
+echo -e "\t\t${text_blue}City Details${text_reset}\n${line}"
 ### get the data in variables
-cname="$( cat "${GEOCODE_JSON_FILE}" | jq '.results[]' | jq '.name' )"
+cname="$( cat "${GEOCODE_JSON_FILE}" | jq '.results[]' | jq '.name' | tr -d '"' )" 
 latitude="$( cat "${GEOCODE_JSON_FILE}" | jq '.results[]' | jq '.latitude' )"
 longitude="$( cat "${GEOCODE_JSON_FILE}" | jq '.results[]' | jq '.longitude' )"
 elevation="$( cat "${GEOCODE_JSON_FILE}" | jq '.results[]' | jq '.elevation' )"
-city_state="$( cat "${GEOCODE_JSON_FILE}" | jq '.results[]' | jq '.admin1' )"
-country="$( cat "${GEOCODE_JSON_FILE}" | jq '.results[]' | jq '.country' )"
+city_state="$( cat "${GEOCODE_JSON_FILE}" | jq '.results[]' | jq '.admin1' | tr -d '"' )"
+country="$( cat "${GEOCODE_JSON_FILE}" | jq '.results[]' | jq '.country' | tr -d '"' )"
 population="$( cat "${GEOCODE_JSON_FILE}" | jq '.results[]' | jq '.population' )"
 
 ### display the output
@@ -61,11 +62,35 @@ echo -e "\tPopulation \t: ${text_cyan}${population}${text_reset}"
 echo "${line}"
 
 ### Now use the latitude and longitude to fetch weather
+echo -e "\t\t${text_blue}Current Weather${text_reset}\n${line}"
 curl "${weatherapi_url}" -G --data-urlencode "latitude=${latitude}" --data-urlencode "longitude=${longitude}" --data-urlencode 'current=temperature_2m,rain,snowfall,wind_speed_10m,relative_humidity_2m' -o "${WEATHER_JSON_FILE}" -s
 
-cat "${WEATHER_JSON_FILE}" | jq
 
-### Cleanup Section
-### remove the temp file created during execution
-# rm "${SCRIPT_DIR}/.geocode_temp.json"
+### collect the data
+temp_unit="$( cat "${WEATHER_JSON_FILE}" | jq '.current_units.temperature_2m' | tr -d '"' )"
+rain_unit="$( cat "${WEATHER_JSON_FILE}" | jq '.current_units.rain' | tr -d '"' )"
+snowfall_unit="$( cat "${WEATHER_JSON_FILE}" | jq '.current_units.snowfall' | tr -d '"' )"
+wind_speed_unit="$( cat "${WEATHER_JSON_FILE}" | jq '.current_units.wind_speed_10m' | tr -d '"' )"
+humidity_unit="$( cat "${WEATHER_JSON_FILE}" | jq '.current_units.relative_humidity_2m' | tr -d '"' )"
 
+temp_val="$( cat "${WEATHER_JSON_FILE}" | jq '.current.temperature_2m' )"
+rain_val="$( cat "${WEATHER_JSON_FILE}" | jq '.current.rain' )"
+snowfall_val="$( cat "${WEATHER_JSON_FILE}" | jq '.current.snowfall' )"
+wind_speed_val="$( cat "${WEATHER_JSON_FILE}" | jq '.current.wind_speed_10m' )"
+humidity_val="$( cat "${WEATHER_JSON_FILE}" | jq '.current.relative_humidity_2m' )"
+
+
+### print the output
+echo -e "\tTemperature\t: ${text_cyan}${temp_val} ${temp_unit}${text_reset}"
+echo -e "\tRainfall\t: ${text_cyan}${rain_val} ${rain_unit}${text_reset}"
+echo -e "\tSnowfall\t: ${text_cyan}${snowfall_val} ${snowfall_unit}${text_reset}"
+echo -e "\tWind Speed\t: ${text_cyan}${wind_speed_val} ${wind_speed_unit}${text_reset}"
+echo -e "\tHumidity\t: ${text_cyan}${humidity_val} ${humidity_unit}${text_reset}"
+
+echo -e "${line}\n\t\t${text_green}Good Day :)${text_reset}\n${line}"
+
+
+### Cleanup Section : remove the api temp files
+rm "${GEOCODE_JSON_FILE}" "${WEATHER_JSON_FILE}"
+
+exit 0
